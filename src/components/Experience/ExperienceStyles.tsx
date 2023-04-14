da03ded8d2e04c9dfb1d3d6c4d8b6e8c7baff1c8
kentakom1213 <kentakom1213@gmail.com>
2023-04-14 16:26:51 +0900

    [add] experienceページの文字をスマホ対応

--- src/components/Experience/ExperienceStyles.tsx
+++ src/components/Experience/ExperienceStyles.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { pc, sp, tab } from "../../styles/Global/media";
 
 export const ExperienceFrame = styled.div`
   margin: 40px 0;
@@ -7,6 +8,13 @@ export const ExperienceFrame = styled.div`
   border: 3px solid #000;
   border-radius: 20px;
   font-size: 20px;
+  ${sp`
+    margin: 30px 0;
+    padding: 20px;
+    border: 2px solid #000;
+    border-radius: 15px;
+    font-size: 15px;
+  `}
 `;
 
 export const ExperienceTitle = styled.h1`
@@ -16,4 +24,8 @@ export const ExperienceTitle = styled.h1`
   padding: 0 10px;
   font-size: 30px;
   background-color: #fff;
+  ${sp`
+    font-size: 20px;
+    top: -15px;
+  `}
 `;
