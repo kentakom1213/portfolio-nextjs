d179a5d9a458d8aac73f6e500bbdb1f27e122aff
kentakom1213 <kentakom1213@gmail.com>
2023-04-14 17:23:06 +0900

    [add] 画像の調整

--- src/components/Experience/ExperienceStyles.tsx
+++ src/components/Experience/ExperienceStyles.tsx
@@ -8,6 +8,9 @@ export const ExperienceFrame = styled.div`
   border: 3px solid #000;
   border-radius: 20px;
   font-size: 20px;
+  ${tab`
+    font-size: 18px;
+  `}
   ${sp`
     margin: 30px 0;
     padding: 20px;
@@ -24,8 +27,25 @@ export const ExperienceTitle = styled.h1`
   padding: 0 10px;
   font-size: 30px;
   background-color: #fff;
+  ${tab`
+    font-size: 25px;
+    top: -20px;
+  `}
   ${sp`
     font-size: 20px;
     top: -15px;
   `}
+`;
+
+export const ExperienceGrid = styled.div`
+  display: grid;
+  position: relative;
+  ${pc`
+    grid-template-columns: 1fr 1fr;
+  `}
+`;
+
+export const ExperienceGridElem = styled.div`
+  height: 300px;
+  position: relative;
 `;
