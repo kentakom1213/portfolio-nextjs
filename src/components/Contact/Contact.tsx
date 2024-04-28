2fa3ffc3415c86fc3b99d545e00eb8c1b6289c17
KOMOTO Kenta <64479799+kentakom1213@users.noreply.github.com>
2024-04-28 10:20:56 +0900

    Contact.tsx の更新
    ブログのリンクを更新

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -7,7 +7,7 @@ export const ContactContents = () => {
     <>
       <ContactIcons>
         <ContactIconLink link="https://github.com/kentakom1213/" src="/images/contact/github-fill.svg" alt="github" />
-        <ContactIconLink link="https://blog.powell1213.com" src="/images/contact/links-fill.svg" alt="blog" />
+        <ContactIconLink link="https://blog.pwll.dev" src="/images/contact/links-fill.svg" alt="blog" />
       </ContactIcons>
     </>
   );
